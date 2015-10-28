cb8be64d26e99378e84288323af8d1953f9a7e67
Ryan Kang <yimingkang@hotmail.com>
2015-10-27 21:40:24 -0400

    keypoint computed and matched

--- Assignment3/recognize.py
+++ Assignment3/recognize.py
@@ -2,10 +2,11 @@ import cv2
 import numpy as np
 from scipy.spatial import distance
 import math
-from matplotlib import pyplot as plt
+#from matplotlib import pyplot as plt
 
 class ImageMatcher():
     def __init__(self, query, db):
+        print "Loading images"
         self.db = cv2.cvtColor(cv2.imread(db), cv2.COLOR_BGR2GRAY)
         self.query = cv2.cvtColor(cv2.imread(query), cv2.COLOR_BGR2GRAY)
 
@@ -13,6 +14,7 @@ class ImageMatcher():
         self.sift = cv2.SIFT()
 
     def compute_keypoints(self):
+        print "Computing keypoints"
         # detect and compute SIFT descriptors
         self.db_kp = self.sift.detectAndCompute(self.db, None)
         self.query_kp = self.sift.detectAndCompute(self.query, None)
@@ -42,6 +44,8 @@ class ImageMatcher():
         ab = np.dot(base_keypoints, query_keypoints.T)
         
         kp_array = a2 + b2 - 2 * ab
+
+        # remove this should have no effect
         kp_array = np.sqrt(kp_array)
 
         (n_val, _) = kp_array.shape
@@ -56,22 +60,37 @@ class ImageMatcher():
 
         ratio = best_match / second_best
         best_ratio = np.amin(ratio)
+        
+        # indicies of base keypoints 
         retained_indicies = np.where(ratio < 0.75)[0]
 
+        # indicies of query keypoints 
+        matched_indicies = best_match_idx[retained_indicies]
+
+        # extract query and base keypoints from keypoint indicies
+        query_kp = map(self.query_kp[0].__getitem__, matched_indicies)
+        base_kp = map(self.db_kp[0].__getitem__, retained_indicies)
+
+        print len(query_kp), ' vs ', len (base_kp)
+
+        print "Query keypoints dim is: ", len(query_kp)
         retained_tuples = sorted([(x, ratio[x]) for x in retained_indicies], key = lambda x: x[1])
-
         print "Best ratio was: ", best_ratio
-        print "List of best matches: \n", retained_tuples
-        #im = cv2.drawKeypoints(self.db[db_idx], good_points)
-        #cv2.imwrite('sift_keypoints.jpg',im)
+        #print "List of best matches: \n", retained_tuples
+        print "Drawing..."
+        im = cv2.drawKeypoints(self.query, query_kp)
+        cv2.imwrite('sift_keypoints.jpg',im)
+
+    def compute_homography(self, db_kp, query_kp):
+        if len(db_kp) != 4 or len(query_kp) != 4:
+            ValueError("Must provide 4 points of {db, query} to compute homography!")
+        
 
 
-    def match_one_pair(self):
-        print "Ecl. distance is ", self.get_euclidian(0)
 
 
 if __name__ == '__main__':
-    im = ImageMatcher("05.jpg", 'toy.jpg')
+    im = ImageMatcher("09.jpg", 'toy.jpg')
     im.compute_keypoints()
     im.get_euclidian()
 
